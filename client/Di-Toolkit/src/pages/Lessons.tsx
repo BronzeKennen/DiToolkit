e8021616247f2703dc6ac14a5f07f8413f8538cd
BronzeKennen <sotoskand3@gmail.com>
2023-07-08 15:14:39 +0300

    test

--- client/Di-Toolkit/src/pages/Lessons.tsx
+++ client/Di-Toolkit/src/pages/Lessons.tsx
@@ -7,7 +7,7 @@ export default function Lessons() {
         return (
             <div>
               {backendData.users.map((user:any, i:any) => (
-                <p key={i}>{user}</p>
+                <p key={i}>{user} cringe cringe cringe</p>
               ))}
             </div>
         );
