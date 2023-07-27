9129aa2f212bb46c01ecfa26cf83490d084be774
bronzekennen <sotoskand3@gmail.com>
2023-07-27 14:25:28 +0300

    Small design changes in courses route, made login screen responsive

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,9 +1,18 @@
 export default function Course( course:any ) {
     return (
-    <div className="container">
-        <h1>{course.lesson_name}</h1>
-        <h2>Course id: {course.cid}</h2>
-        <h2>Description: {course.description}</h2>
+    <div>
+        <h1>{course.lesson_name} | {course.cid}</h1>
+        <div className="course-container">
+            <nav className="course-nav">
+                <ul>
+                    <a>About</a>
+                    <a>Palia themata</a>
+                </ul>
+                <div className="course-about">
+                    <p>{course.description}</p>
+                </div>
+            </nav>
+        </div>
     </div>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,29 @@
+import { motion } from 'framer-motion'
+
 export default function Login() {
     return (
     <div className="loginBox">
         <form className="login">
-            <label>Username</label>
+            <label>e-class username</label>
             <input className="credsBox"
                 type="text"
                 />
-            <label>Password</label>
+            <label>e-class password</label>
             <input className="credsBox"
-                type="text"
-            />
+                type="password"
+            /><br></br>
+            <motion.button 
+            type='button'
+            className="loginButton"
+            whileHover={{
+                scale: 1.01,
+                boxShadow: "0px 0px 10px green",
+                textShadow: "0px 0px 12px red"
+            }}>Login</motion.button>
+            <div className="test">
+                <a text-align="left">Forgot Password?</a>
+                <a text-align="right">Terms & Conditions</a>
+            </div>
         </form>
     </div>
     )
